9a8540c38e04827bd13137d7085eefe3359928c8
Ghayas Ali <55618250+ghayas110@users.noreply.github.com>
2024-05-21 20:48:47 +0500

    ssef

--- ios/Bartender_App/AppDelegate.mm
+++ ios/Bartender_App/AppDelegate.mm
@@ -1,7 +1,7 @@
 #import "AppDelegate.h"
 #import <Firebase.h>
 #import <React/RCTBundleURLProvider.h>
-
+#import <FIRApp.h>
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
